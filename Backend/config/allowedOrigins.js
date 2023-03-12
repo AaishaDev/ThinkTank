f545347711829780550403d7649a9e4274b30b7c
AaishaDev <aaishllap@gmail.com>
2023-03-12 13:33:46 +0530

    allowed origin add

--- Backend/config/allowedOrigins.js
+++ Backend/config/allowedOrigins.js
@@ -3,7 +3,7 @@ const allowedOrigins = [
     'http://127.0.0.1:5000',
     'http://localhost:3500',
     'http://localhost:3000',
-    'https://think-tank-notes.netlify.app'
+    "https://think-tank-notes.netlify.app/"
 ];
 
 module.exports = allowedOrigins;
